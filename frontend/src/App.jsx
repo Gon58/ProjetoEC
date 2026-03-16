deeccd722bc2628767d20759913ff64273118e9d
Fernando Pires <ferjpires@gmail.com>
2026-03-16 13:21:26 +0000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+
 export default function App() {
+  const [page, setPage] = useState("home");
+
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>EC Project</h1>
-      <p>Frontend MVP is running 🚀</p>
-    </div>
-  )
+    <MainLayout currentPage={page} onNavigate={setPage}>
+      {page === "home" && <HomePage onEnterDashboard={() => setPage("dashboard")} />}
+      {page === "dashboard" && <DashboardPage />}
+    </MainLayout>
+  );
 }
